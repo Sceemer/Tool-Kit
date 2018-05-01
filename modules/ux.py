eb478ff5772e600a0d791b9910c60cd19a0cafda
Rajkumar Dusad <32831684+Rajkumrdusad@users.noreply.github.com>
2018-05-01 09:47:54 +0530

    Add files via upload

--- modules/ux.py
+++ modules/ux.py
@@ -1,1 +1,13 @@
+# Tool Name :- Tool Kit
+# Author :- Rajkumar Dusad
+# Date :- 24/11/2017
+# Powered By :- Aex Software's
 
+import os
+import sys
+from time import sleep
+
+def Ux():
+ Ux = os.system("clear")
+def ex():
+ ex = sys.exit()
